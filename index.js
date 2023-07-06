d888a4b347f6b885efee6511bd17e8bf4ba11553
Ryan Lee <ryan@parity.io>
2023-07-06 13:45:26 -0400

    feat: support capi env variables

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 import { Transformer } from "@parcel/plugin";
 
 export default new Transformer({
-  async transform({ asset }) {
+  async transform({ asset, options }) {
     const code = await asset.getCode();
-    const result = code.replace(/import (.*?) \"node:fs\"(.*?)\n/g, () => "");
+
+    const result = code
+      .replace(/import (.*?) \"node:fs\"(.*?)\n/g, () => "")
+      .replace(
+        /tryGetEnv\(\"CAPI_SERVER\"\)/g,
+        () => `\"${options.env.CAPI_SERVER}\"`
+      )
+      .replace(
+        /tryGetEnv\(\"CAPI_TARGET\"\)/g,
+        () => `\"${options.env.CAPI_TARGET}\"`
+      );
 
     asset.setCode(result);
     return [asset];
