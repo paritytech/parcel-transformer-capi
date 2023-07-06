ef055f6a29aa2d82922a62219cff2de43bfa1f42
Ryan Lee <ryan@parity.io>
2023-07-06 16:52:48 -0400

    fix: filter for capi files only (#1)

--- index.js
+++ index.js
@@ -2,20 +2,25 @@ import { Transformer } from "@parcel/plugin";
 
 export default new Transformer({
   async transform({ asset, options }) {
-    const code = await asset.getCode();
+    const isCapiFile = asset.filePath.match(/capi\/esm\//g);
 
-    const result = code
-      .replace(/import (.*?) \"node:fs\"(.*?)\n/g, () => "")
-      .replace(
-        /tryGetEnv\(\"CAPI_SERVER\"\)/g,
-        () => `\"${options.env.CAPI_SERVER}\"`
-      )
-      .replace(
-        /tryGetEnv\(\"CAPI_TARGET\"\)/g,
-        () => `\"${options.env.CAPI_TARGET}\"`
-      );
+    if (isCapiFile) {
+      const code = await asset.getCode();
 
-    asset.setCode(result);
+      const result = code
+        .replace(/import (.*?) \"node:fs\"(.*?)\n/g, () => "")
+        .replace(
+          /tryGetEnv\(\"CAPI_SERVER\"\)/g,
+          () => `\"${options.env.CAPI_SERVER}\"`
+        )
+        .replace(
+          /tryGetEnv\(\"CAPI_TARGET\"\)/g,
+          () => `\"${options.env.CAPI_TARGET}\"`
+        );
+
+      asset.setCode(result);
+    }
+
     return [asset];
   },
 });
